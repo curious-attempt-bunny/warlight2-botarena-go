015f111911e38d100a94bbaba3a737251419b74e
Merlyn Albery-Speyer <merlyn@newrelic.com>
2015-03-10 17:23:54 -0700

    Launch a bot process.

--- arena.go
+++ arena.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import exec "os/exec"
 
+type Bot struct {
+    process *exec.Cmd
+}
+
+func main() {
+    launch("broken_bot.sh")
+}
+
+func launch(launch_script string) *Bot {
+    cmd := exec.Command(launch_script)
+
+    bot := &Bot{process: cmd}
+
+    return bot
 }
